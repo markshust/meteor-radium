ec35fab7efb40314467b30192013b33cb1560cc5
Mark Shust <mark@shust.com>
2015-11-18 08:50:39 -0500

    Updated to Radium 0.15.3 and browserify 0.8.4 #5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'markoshust:radium',
-  version: '0.14.3',
+  version: '0.15.3',
   summary: 'Add the React Radium package to Meteor',
   git: 'https://github.com/markoshust/meteor-radium.git',
   documentation: 'README.md'
@@ -8,13 +8,13 @@ Package.describe({
 
 Npm.depends({
   'externalify': '0.1.0',
-  'radium': '0.14.3'
+  'radium': '0.15.3'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use([
-    'cosmos:browserify@0.8.1',
+    'cosmos:browserify@0.8.4',
     'react@0.14.1_1'
   ]);
   api.addFiles([
